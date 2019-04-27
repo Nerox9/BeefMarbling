5512f832ac1a9208bbd2d4699a63808aa7fc4ba2
Berk Arslan <arslanberk09@gmail.com>
2019-04-27 23:10:46 +0300

    Matlab code is updated

--- marbling.m
+++ marbling.m
@@ -1,16 +1,21 @@
 addpath('Scripts');
 dataset_path = fullfile(pwd, 'Beef Dataset');
-image_path = fullfile(dataset_path, '0.jpg');
+image_path = fullfile(dataset_path, 'Choice-Moderate0.png');
 
-% Get image
+%% Get image
 image = imread(image_path);
 padded = padarray(image,[3 3],'replicate','both');
 gray = rgb2gray(padded);
 
-% Median Blurring
+%% Median Blurring
 blurred = medfilt2(gray, [5 5]);
 unpadded_size = size(blurred);
 blurred = blurred(4:unpadded_size(1)-3, 4:unpadded_size(2)-3);
 
 %% Region Growing method
-[P J]=regionGrowing(gray)
+background = regionGrowing(double(blurred),unpadded_size(1)-6,unpadded_size(2)-6,10);
+
+%% Thresholding
+otsulevel = graythresh(blurred);
+binimage = im2bw(blurred,otsulevel);
+imshow(binimage.*(1-background))
