c73a9323fc50555d73498c8172f5e554c0960b6f
Burak <Bciceksoy@users.noreply.github.com>
2019-05-01 17:54:24 +0300

    Add files via upload

--- marbling.m
+++ marbling.m
@@ -1,6 +1,6 @@
 addpath('Scripts');
 dataset_path = fullfile(pwd, 'Beef Dataset');
-image_path = fullfile(dataset_path, 'Choice-Moderate0.png');
+image_path = fullfile(dataset_path, 'Choice-Moderate_havuz.png');
 
 %% Get image
 image = imread(image_path);
@@ -8,7 +8,7 @@ padded = padarray(image,[3 3],'replicate','both');
 gray = rgb2gray(padded);
 
 %% Median Blurring
-blurred = medfilt2(gray, [5 5]);
+blurred = medfilt2(gray, [3 3]);
 unpadded_size = size(blurred);
 blurred = blurred(4:unpadded_size(1)-3, 4:unpadded_size(2)-3);
 
@@ -20,25 +20,52 @@ otsulevel = graythresh(blurred);
 binimage = im2bw(blurred,otsulevel);
 
 %% Image Subtraction
-fatimage = binimage.*(1-background);
+fatimage = binimage.*(~background);
 meatTissue = xor(fatimage,~background);
-%outfat = meatTissue - 
 
 %% Print
-subplot(4,2,1)
+subplot(2,5,1)
 imshow(image)
-subplot(4,2,2)
+title('(1) Original Image')
+subplot(2,5,2)
 imshow(gray)
-subplot(4,2,3)
+title('(2) Grayscale Image')
+subplot(2,5,3)
 imshow(blurred)
-subplot(4,2,4)
+title('(3) Median Blurring')
+subplot(2,5,4)
 imshow(background)
-subplot(4,2,5)
+title('(4) Background(Region Growing Method)')
+subplot(2,5,5)
 imshow(binimage)
-subplot(4,2,6)
+title('(5) Binarisation')
+subplot(2,5,6)
 imshow(fatimage)
-subplot(4,2,7)
-imshow(meatTissue)
+title('(6) 5 * ~4')
+subplot(2,5,7)
+meatimage = bwareafilt(~binimage,1);
+imshow(meatimage)
+title('(7) Only Biggest Area Remains of ~6')
+subplot(2,5,8)
+filledmeat = imfill(meatimage, 'holes');
+imshow(filledmeat)
+title('(8) Fill Holes')
+subplot(2,5,9)
+onlyfat = and(filledmeat, fatimage);
+imshow(onlyfat)
+title('(9) 8 AND 6')
+subplot(2,5,10)
+onlymeat = xor(onlyfat, filledmeat);
+imshow(onlymeat)
+title('(10) 9 XOR 8')
 
+fatcount = bwconncomp(onlyfat)
+fatcount.NumObjects
+
+fatarea = bwarea(onlyfat)
+
+meatarea = bwarea(onlymeat)
+
+fattomeatratio = fatarea/meatarea
 %% BPNN
-train_bpnn(0)
+%%train_bpnn(0)
