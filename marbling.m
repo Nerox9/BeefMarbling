098404a4c4be730c77132696467c01531e391729
Berk Arslan <neroxx@windowslive.com>
2019-05-01 15:50:51 +0300

    Meat Xor update

--- marbling.m
+++ marbling.m
@@ -18,4 +18,27 @@ background = regionGrowing(double(blurred),unpadded_size(1)-6,unpadded_size(2)-6
 %% Thresholding
 otsulevel = graythresh(blurred);
 binimage = im2bw(blurred,otsulevel);
-imshow(binimage.*(1-background))
+
+%% Image Subtraction
+fatimage = binimage.*(1-background);
+meatTissue = xor(fatimage,~background);
+%outfat = meatTissue - 
+
+%% Print
+subplot(4,2,1)
+imshow(image)
+subplot(4,2,2)
+imshow(gray)
+subplot(4,2,3)
+imshow(blurred)
+subplot(4,2,4)
+imshow(background)
+subplot(4,2,5)
+imshow(binimage)
+subplot(4,2,6)
+imshow(fatimage)
+subplot(4,2,7)
+imshow(meatTissue)
+
+%% BPNN
+train_bpnn(0)
